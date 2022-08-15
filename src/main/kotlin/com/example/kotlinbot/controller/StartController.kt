d5e287f697a2a4a44e646b559646ebc2a0785039
Alexandr Maikevich <72385550+Maikevich@users.noreply.github.com>
2022-08-15 18:33:01 +0200

    controller added

---
+++ src/main/kotlin/com/example/kotlinbot/controller/StartController.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinbot.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/start")
+class StartController {
+
+    @GetMapping(value = ["/"])
+    @ResponseBody
+    fun hello(): String? ="Get back to work"
+
+
+}
